a49b9eed9d8a79dfb920b5490fad7d8687687511
Peter Ni <shuaiqidepeterni@gmail.com>
2023-03-30 10:30:07 -0700

    flaskapp

--- Web/main.js
+++ Web/main.js
@@ -1,11 +1,15 @@
-document.onload
-
-document.getElementById("calibrate-arm").onclick = function () {
-    // do stuff
-    console.log("Calibrate Arm");
-}
-
-document.getElementById("sort-all").onclick = function () {
-    // do stuff
-    console.log("Sort All");
-}
+document.getElementById("calibrate-arm").onclick = function() {
+    // Send an HTTP POST request to the Flask server
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://127.0.0.1:5000/calibrate-arm", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // Update the HTML page with the detected objects
+        var results = JSON.parse(xhr.responseText);
+        // Do something with the results...
+        print("hello");
+      }
+    };
+    xhr.send(JSON.stringify({}));
+  };

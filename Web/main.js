7f2f3569c50b2b3167a0e3a49642ca502fb76654
Wesley Chow <wgesh8@gmail.com>
2023-03-30 01:12:42 -0700

    update web design and add js clicks

---
+++ Web/main.js
@@ -0,0 +1,11 @@
+document.onload
+
+document.getElementById("calibrate-arm").onclick = function () {
+    // do stuff
+    console.log("Calibrate Arm");
+}
+
+document.getElementById("sort-all").onclick = function () {
+    // do stuff
+    console.log("Sort All");
+}

e52e59d5e0cfb8c343302668216bfc5e6c941914
Peter Ni <shuaiqidepeterni@gmail.com>
2023-04-01 18:31:21 -0700

    1

--- Flask/flaskapp.py
+++ Flask/flaskapp.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from testing import SortingArm
 
 app = Flask(__name__)
+arm = SortingArm()
 
 @app.route('/calibrate')
 def calibrate():
-    # Your code to receive the signal on your laptop goes here
+    arm.calibrate_arm()
     return 'Calibration signal received'
+
+@app.route('/sort-all')
+def sort_all():
+    arm.sort()
+    return 'Sort all signal received'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
